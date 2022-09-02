0fb8344015824b4fb7b1ee263aa4b8dd7e0b713a
Satyr <elflord@faerie.cx>
2022-09-02 23:55:22 +1000

    michy def prt2

--- cointest/script.py
+++ cointest/script.py
@@ -1,2 +1,50 @@
-print ("bob")
-print ("no soup for you!")
+from audioop import reverse
+from doctest import OutputChecker
+from re import A
+
+
+outer_code = ['URMWXOZIRGBRM7DRWGSC5WVKGS','DVZIVZFWZXRLFHRMXLMXVKGZMWNVGRXFOLFHRMVCVXFGRLN']
+
+'''staright_alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+reversed_alpha = staright_alpha[::-1]
+
+
+
+result = ""
+
+for i in outer_code[0]:
+    if staright_alpha.find(i) == -1:
+        result = result + i
+    else:
+        pos = staright_alpha.find(i)
+        result = result + reversed_alpha[pos]
+    
+print(result)
+
+
+result = ""
+for i in outer_code[1]:
+    pos = staright_alpha.find(i)
+    result = result + reversed_alpha[pos]
+    
+print(result)'''
+
+def decode_print(input):
+    staright_alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    reversed_alpha = staright_alpha[::-1]
+
+    result = ""
+
+    for i in input:
+        if staright_alpha.find(i) == -1:
+            result = result + i
+        else:
+            pos = staright_alpha.find(i)
+            result = result + reversed_alpha[pos]
+       
+
+    return result
+
+for b in outer_code:
+    print(decode_print(b))
+
